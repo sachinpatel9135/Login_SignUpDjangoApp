565abd63825199c9c81250a96c3af1f38bacbc7d
Sachin Patel <36100562+sachinpatel9135@users.noreply.github.com>
2021-05-07 23:30:36 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mysite',
         'USER': 'postgres',
-        'PASSWORD': '8853304808786',
+        'PASSWORD': '*************',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
